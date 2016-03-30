5be6d6763c6117c1d598ae608fc7fae880e97f38
zhaojuntao <2849324422@qq.com>
2016-03-30 16:22:43 +0800

    CRC8 AND CRC16 Source File
    V1.0

---
+++ CRC8_16/crc8_16.h
@@ -0,0 +1,36 @@
+#ifndef __CRC8_16_H
+#define __CRC8_16_H
+#include "stdint.h"	 
+#include "mico.h"
+uint16_t Get_Crc16(uint8_t *puchMsg,uint16_t usDataLen);
+uint8_t Get_Crc8(uint8_t *ptr,uint16_t len);
+uint16_t CRC16(uint8_t* pdata, uint16_t datalen);
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
